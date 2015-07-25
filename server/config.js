f637179a2667d2b70d9e685b4b06129c86a96b0b
Fabien BEHIER <conker14000@gmail.com>
2015-07-25 09:29:48 +0100

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
-process.env.MAIL_URL = 'smtp://postmaster%40sandboxca79aa82946f4dae955b96b830c6a598.mailgun.org:_Ou1_Ou1@smtp.mailgun.org:587';
+process.env.MAIL_URL = 'smtp://postmaster%40sandboxca79aa82946f4dae955b96b830c6a598.mailgun.org:PASSWORD@smtp.mailgun.org:587';
 
 AccountsTemplates.configure({
     reCaptcha: {
