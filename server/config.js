36c9a3f48047e179bf0bcafa66159876e7435871
Fabien BEHIER <conker14000@gmail.com>
2015-07-25 09:30:09 +0100

    Update config.js

--- server/config.js
+++ server/config.js
@@ -2,6 +2,6 @@ process.env.MAIL_URL = 'smtp://postmaster%40sandboxca79aa82946f4dae955b96b830c6a
 
 AccountsTemplates.configure({
     reCaptcha: {
-        secretKey: '6LeRowcTAAAAAITLjSBT2l3nK3xXCdT4m4H6q1th'
+        secretKey: 'YOUR_KEY'
     },
 });
